c927dae1e334b121102b830b0bd02e2fffac3784
piyushh-ai <thepiyushsirolia@gmail.com>
2026-04-21 09:39:20 +0530

    last commit

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -15,7 +15,12 @@ const transporter = nodemailer.createTransport({
 const generateToken = (id, res, message, user) => {
   const token = jwt.sign({ id }, config.jwt_secret, { expiresIn: "7d" });
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+  });
   res.status(201).json({ message: message, user });
 };
 
